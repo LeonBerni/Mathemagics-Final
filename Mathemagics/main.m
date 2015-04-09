ae17b27d8c36660d70b1382e6c6e595fa9c4b33d
Leon Berni <leonberni@hotmail.com>
2015-04-09 14:20:52 -0300

    Initial Commit

---
+++ Mathemagics/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Mathemagics
+//
+//  Created by Leon Berni on 4/9/15.
+//  Copyright (c) 2015 Leon Berni & Gabriel Nopper. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
